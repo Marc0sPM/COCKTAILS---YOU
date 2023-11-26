12a3e867ea3fff916d12f4544221d57db691992b
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-11-26 16:29:03 +0100

    Falta q funcionen las colisiones

--- src/scenes/EscenaVivuPrueba.js
+++ src/scenes/EscenaVivuPrueba.js
@@ -13,13 +13,15 @@ export default class EscenaVivuPrueba extends Phaser.Scene{
     create(){
         //Se agregan fisicas a la escena
         this.physics.world.setBounds(0, 0, this.sys.game.config.width, this.sys.game.config.height);
-        //intancia el player
+        // Configurar jugador
         this.player = new Player(this, 300, 300);
-        //se agrega player a escena
-        
+        this.physics.world.enable(this.player);  // Habilitar físicas para el jugador
+        this.player.setCollideWorldBounds(true);  // Asegurar que el jugador no salga de los límites del mundo
 
         console.log('create EscenaVivuPrueba')
 
+
+        // Tilemap
         let map = this.add.tilemap("barTiled");
 
         let floor = map.addTilesetImage("floorTiles", "floor");
@@ -32,6 +34,19 @@ export default class EscenaVivuPrueba extends Phaser.Scene{
         let wallLayer = map.createLayer("Varios", barObjects, 0, 0);
         let woodLayer = map.createLayer("Pared", barObjects, 0, 0);
         let variousLayer = map.createLayer("Madera", barObjects, 0, 0);
+
+        // Profundidad de las capas
+        groundLayer.setDepth(0);
+        wallLayer.setDepth(1);
+        woodLayer.setDepth(2);
+        variousLayer.setDepth(3);
+        
+        this.player.setDepth(4);
+
+        // Colisiones del Player con la escena
+        this.physics.add.collider(this.player, wallLayer);
+        this.physics.add.collider(this.player, woodLayer);
+        this.physics.add.collider(this.player, variousLayer);
     }
     update(){
         this.player.update();
