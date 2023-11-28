e96a3b43f729374452a9a20b94081407b543dd94
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-11-28 11:37:21 +0100

    Cambio del player para q hayan collisiones con el mapa, spoiler, no hay

--- src/scenes/player.js
+++ src/scenes/player.js
@@ -1,59 +1,59 @@
-let lookingRight = Boolean(true); 
+let lookingRight = true;
 
-
-export default class Player extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y){
-        super(scene, x, y, {key: 'player'})
+export default class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, { key: 'player' });
         this.scene.add.existing(this);
         this.scene.physics.world.enable(this);
         this.setScale(2);
-        
+
+        // Ajustar el tamaño del cuerpo de físicas para que coincida con el sprite visual
+        this.body.setSize(48, 48);
+
         this.cursors = scene.input.keyboard.addKeys({
-             up: Phaser.Input.Keyboard.KeyCodes.W,
-             down: Phaser.Input.Keyboard.KeyCodes.S,
-             left: Phaser.Input.Keyboard.KeyCodes.A,
-             right: Phaser.Input.Keyboard.KeyCodes.D
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D
         });
 
-        //Velocidad del jugador
+        // Velocidad del jugador
         this.speed = 200;
     }
-    update( ){
+
+    update() {
         if (this.cursors.up.isDown) {
-
             this.setVelocityY(-this.speed);
-            
-            this.anims.play('player_walk', true)
-
+            this.anims.play('player_walk', true);
         } else if (this.cursors.down.isDown) {
             this.setVelocityY(this.speed);
-            this.anims.play('player_walk', true)
+            this.anims.play('player_walk', true);
         } else {
             this.setVelocityY(0);
         }
 
         if (this.cursors.left.isDown) {
             this.setVelocityX(-this.speed);
-            this.anims.play('player_walk', true)
-            //si mira a la derecha que haga el flip y calcule la nueva posicion sin salto
-            if(lookingRight){
-                lookingRight = Boolean(false);
+            this.anims.play('player_walk', true);
+            if (lookingRight) {
+                lookingRight = false;
                 this.setFlipX(true);
-                this.x -= 48 //es lo que mide el sprite
+                this.x -= 48; // Ajusta según el tamaño del sprite
             }
         } else if (this.cursors.right.isDown) {
-            this.anims.play('player_walk', true)
+            this.anims.play('player_walk', true);
             this.setVelocityX(this.speed);
-            if(!lookingRight){
-                lookingRight = Boolean(true);
-                this.setFlip(false)
-               this.x += 48 //es lo que mide el sprite
-                
+            if (!lookingRight) {
+                lookingRight = true;
+                this.setFlipX(false);
+                this.x += 48; // Ajusta según el tamaño del sprite
             }
-        }else this.setVelocityX(0)
-        if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
-            this.anims.play('player_idle', true)
+        } else {
+            this.setVelocityX(0);
         }
-        
+
+        if (this.body.velocity.x === 0 && this.body.velocity.y === 0) {
+            this.anims.play('player_idle', true);
+        }
     }
 }
