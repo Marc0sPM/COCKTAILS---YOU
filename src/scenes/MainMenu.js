0096f48767f1cffae1244c7ed710497ba99140e6
[MarcosPRC] <marcopan@ucm.es>
2023-12-13 22:40:18 +0100

    brooooooo mi boton del breakout

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -28,15 +28,15 @@ class MainMenu extends Phaser.Scene{
 
         //-------------------------------------------------//
         // Botón Breakout
-        //let buttonBreakout = this.add.image(this.sys.game.config.width/4, 
-        //this.sys.game.config.height/2 + this.sys.game.config.height/3, "playButton");
-        //buttonBreakout.setScale(0.25);
-        //buttonBreakout.setInteractive();
+        let buttonBreakout = this.add.image(this.sys.game.config.width/4, 
+        this.sys.game.config.height/2 + this.sys.game.config.height/3, "playButton");
+        buttonBreakout.setScale(0.25);
+        buttonBreakout.setInteractive();
         // Escucha el evento "pointerdown" en el botón
-       // buttonBreakout.on("pointerdown", () => {
-            // Carga la escena que desees al hacer clic en el botón
-           // this.scene.start('Breakout');
-       // });
+        buttonBreakout.on("pointerdown", () => {
+            //Carga la escena que desees al hacer clic en el botón
+           this.scene.start('Breakout');
+        });
         //-------------------------------------------------//
 
 
