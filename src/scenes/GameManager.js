8bcc025321b48b42fd686c12af8a33e0d5a0cc54
Marc0sPM <marcpe03@ucm.es>
2023-12-18 18:13:52 +0100

    puntuaciones y medias de los niveles mas o menos establecidas en el gameManager

--- src/scenes/GameManager.js
+++ src/scenes/GameManager.js
@@ -1,25 +1,70 @@
-
+//"Especia" del coctel actual
 export var other = ''
 export function setOther(num){
     other = num
 }
-
-export default class GameManager {
-    othersCant = 0
-    addOther(cant){
-        othersCant = cant;
-    }
-    totalStars = 0
-    maxStars = 0
-    addMaxStars(num){
-        this.maxStars += num
-    }
-    addStart(num){
-        this.totalStars+= num
-    }
-    other = ' '
-    setOther(h){
-        this.other = h
-    }
+//Refresco del coctel  actual
+export var refreshment = ''
+export function setRefreshment(name){
+    refreshment = name
 }
+//Alcohol del coctel actual 
+export var alcohol = ''
+export function setAlcohol(name){
+    alcohol = name
+}
+
+//Customers totales del nivel
+export var numCustomers = 0;
+//Estrellas totales de cada customer de forma inividual
+export var currentCustomerPoints = 0;
+//Media de las estrellas totales de cada customer
+export var currentCustomersStars = 0;
+//Lista de estrellas totales de cada customer
+export var individualTotalStars = [];
+
+//Establece cantidad total de customers
+export function setNumCustomers(num){
+    numCustomers = num
+}
+
+//Despues de calcular la puntuacion de cada minijuago, se agregar a la variable
+export function addCustomerPoints(points){
+    currentCustomerPoints += points
+}
+
+//Hace la media entre la puntuacion de estrellas y los minijuegos que se han jugado
+// y lo añade a la lista de estrellas
+export function calculateCustomerStarts(cantMinigames){
+    currentCustomersStars = currentCustomerPoints / cantMinigames
+    addTotalIndivStars(currentCustomerPoints);
+}
+
+
+//En caso de tener distintos customers, 
+//tener la puntuacion total de cada uno de ellos de forma individual
+//Añade una puntuacion a la lista de puntuaciones
+export function addTotalIndivStars(numStars){
+    individualTotalStars.push(numStars)
+}
+
+//Estrellas que se va a mostrar en pantalla al final del nivel
+export var finalStars = 0;
+//Calcula la media de estrellas final
+export function calculateLevelStars(){
+    var rate = 0
+    individualTotalStars.forEach(cust => {
+        rate += cust
+    })
+    finalStars = rate / numCustomers;
+}
+
+//Info de nivel
+export const levels = {
+    'level1' : {customerCant: 1},
+    'level2' : {customerCant: 2},
+    'level3' : {customerCant: 3},
+    'level4' : {customerCant: 4}
+}
+
 
