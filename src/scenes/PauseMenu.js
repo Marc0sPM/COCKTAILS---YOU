2d6e071a364800d6e73c9143f272daf7c6c00d35
[MarcosPRC] <marcopan@ucm.es>
2023-12-20 02:41:30 +0100

    cambio offset botones

--- src/scenes/PauseMenu.js
+++ src/scenes/PauseMenu.js
@@ -14,15 +14,15 @@ class PauseMenu extends Phaser.Scene {
         this.texto = this.add.image(400,300,currenttext).setDepth(0);
         this.texto.setScale(0.5)
 
-        this.resume = new Button(this,550, 270, 0.25,()=>{
+        this.resume = new Button(this,400, 570, 0.25,()=>{
         this.scene.stop()
         this.scene.resume(currentminigame)
         this.sound.resumeAll();
          },'resumeButton');
 
-        this.volextra = new Button(this,490, 340, 0.12,()=>{
+        this.volextra = new Button(this,350, 510, 0.12,()=>{
              },'resumeButton');
-        this.volemenos = new Button(this,610, 340, 0.12,()=>{
+        this.volemenos = new Button(this,450, 510, 0.12,()=>{
             },'resumeButton');
             
         
